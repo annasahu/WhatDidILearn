02a0261471658085f31c0b4c810321d74ab94369
Anna Sahu <73133104+annasahu@users.noreply.github.com>
2020-10-31 15:23:00 -0300

    adding adapter

--- app/src/main/java/com/wcc/whatdidilearn/view/MainActivity.kt
+++ app/src/main/java/com/wcc/whatdidilearn/view/MainActivity.kt
@@ -2,11 +2,21 @@ package com.wcc.whatdidilearn.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 import com.wcc.whatdidilearn.R
+import com.wcc.whatdidilearn.data.DataBaseItems
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recycleView = findViewById<RecyclerView>(R.id.items_recycler_view)
+        val adapter = LearnedItemsAdapter()
+
+        recycleView.adapter = adapter
+
+        val learnedItemsList = DataBaseItems().getAll()
+            adapter.data = learnedItemsList
     }
 }
