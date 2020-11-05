3ea39fb6aa1ce201ba8bd1ed41bf565c673e4adb
Anna Sahu <73133104+annasahu@users.noreply.github.com>
2020-11-04 21:23:13 -0300

    populate database

--- app/src/main/java/com/wcc/whatdidilearn/view/MainActivity.kt
+++ app/src/main/java/com/wcc/whatdidilearn/view/MainActivity.kt
@@ -2,8 +2,12 @@ package com.wcc.whatdidilearn.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import androidx.lifecycle.Observer
 import com.wcc.whatdidilearn.data.DatabaseItems
 import com.wcc.whatdidilearn.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,7 +20,11 @@ class MainActivity : AppCompatActivity() {
 
         recycleView.adapter = adapter
 
-        val learnedItemsList = DatabaseItems.getAll()
-            adapter.data = learnedItemsList
+        val database = DatabaseItems.getDatabase(this, CoroutineScope(Dispatchers.IO))
+        val dao = database.learnedItemDao()
+        val itemsList = dao.getAll()
+        itemsList.observe(this, Observer { items ->
+            adapter.data = items
+        })
     }
 }
